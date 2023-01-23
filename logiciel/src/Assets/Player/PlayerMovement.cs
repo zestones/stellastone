f1f59a5d501069047137c8696bb4d0a9d0999a3b
Idriss <idrissbenguezzou@gmail.com>
2023-01-23 19:23:51 +0100

    feat: init project

---
+++ logiciel/src/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space")) {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 5, 0);
+        }   
+    }
+}
