24f5e0a1b84acfc1fd1e7b3e1e2703f58b6627c7
Idriss <idrissbenguezzou@gmail.com>
2023-03-14 16:01:58 +0100

    feat: add dock and redirect scene

--- logiciel/Assets/Scripts/UI/Button/CustomUIButtonGroup.cs
+++ logiciel/Assets/Scripts/UI/Button/CustomUIButtonGroup.cs
@@ -3,33 +3,37 @@ using UnityEngine.EventSystems;
 
 public class CustomUIButtonGroup : MonoBehaviour
 {
-    private CustomUITextButton[] buttons;
+	private CustomUITextButton[] textButtons;
 
-    private void Start()
-    {
-        // récupère tous les scripts CustomUITextButton attachés aux boutons enfants
-        buttons = GetComponentsInChildren<CustomUITextButton>();
-    }
+	private void Start()
+	{
+		// récupère tous les scripts CustomUITextButton attachés aux boutons enfants
+		textButtons = GetComponentsInChildren<CustomUITextButton>();
+	}
 
-    public void OnButtonSelect(CustomUITextButton selectedButton)
-    {
-        foreach (CustomUITextButton button in buttons)
-        {
-            if (button != selectedButton)
-            {
-                button.isSelected = false;
-                button.OnDeselect(null); // appelle la méthode OnDeselectHandler pour réinitialiser la couleur du texte
-                if (button.objectToToggle != null)
-                {
-                    button.objectToToggle.SetActive(false);
-                }
-            }
-            else {
-                if (button.objectToToggle != null)
-                {
-                    button.objectToToggle.SetActive(true);
-                }
-            }
-        }
-    }
+	public void OnButtonSelect(CustomUITextButton selectedButton)
+	{
+		foreach (CustomUITextButton button in textButtons)
+		{
+			if (button != selectedButton)
+			{
+				button.IsSelected = false;
+				button.OnDeselect(null); // appelle la méthode OnDeselectHandler pour réinitialiser la couleur du texte
+				if (button.objectToToggle != null)
+				{
+					button.objectToToggle.SetActive(false);
+				}
+			}
+			else {
+				if (button.objectToToggle != null)
+				{
+					button.objectToToggle.SetActive(true);
+				}
+				else if (button.sceneName != null) 
+				{
+					button.ChangeScene();
+				}
+			}
+		}
+	}
 }
--- logiciel/Assets/Scripts/UI/Button/CustomUITextButton.cs
+++ logiciel/Assets/Scripts/UI/Button/CustomUITextButton.cs
@@ -1,72 +1,89 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class CustomUITextButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler, IPointerClickHandler
 {
-    public GameObject objectToToggle; // GameObject à activer/désactiver
-    private Text buttonText;
-    private Color normalColor;
-    public Color hoverColor;
-    public Color selectedColor;
-    public bool isHovered = false;
-    public bool isSelected = false;
-    public bool isInitiallySelected;
-    private CustomUIButtonGroup buttonGroup;
+	public Color hoverColor;
+	public Color selectedColor;
+	public GameObject objectToToggle; // GameObject à activer/désactiver
+	public bool isInitiallySelected;
+	
+	public string sceneName;
+	
+	private Text buttonText;
+	private Color normalColor;
+	
+	private bool isHovered = false;
+	private bool isSelected = false;
+		
+	private CustomUIButtonGroup buttonGroup;
 
-    private void Start()
-    {
-        buttonText = GetComponentInChildren<Text>();
-        normalColor = buttonText.color;
-        buttonGroup = GetComponentInParent<CustomUIButtonGroup>();
-        
-        if (isInitiallySelected)
-        {
-            OnSelect(null);
-        }
-    }
+	public bool IsHovered { get { return isHovered; } }
+	public bool IsSelected
+	{
+		get { return isSelected; }
+		set { isSelected = value; }
+	}
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        if (!isSelected) // Ne modifiez pas la couleur si le bouton est sélectionné
-        {
-            buttonText.color = hoverColor;
-            isHovered = true;
-        }
-    }
+	private void Start()
+	{
+		buttonText = GetComponentInChildren<Text>();
+		normalColor = buttonText.color;
+		buttonGroup = GetComponentInParent<CustomUIButtonGroup>();
+		
+		if (isInitiallySelected)
+		{
+			OnSelect(null);
+		}
+	}
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        if (!isSelected) // Ne modifiez pas la couleur si le bouton est sélectionné
-        {
-            buttonText.color = normalColor;
-            isHovered = false;
-        }
-    }
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		if (!isSelected) // Ne modifiez pas la couleur si le bouton est sélectionné
+		{
+			buttonText.color = hoverColor;
+			isHovered = true;
+		}
+	}
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if (!isSelected)
-        {
-            OnSelect(eventData);
-            if (buttonGroup != null)
-            {
-                buttonGroup.OnButtonSelect(this);
-            }
-        }
-    }
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		if (!isSelected) // Ne modifiez pas la couleur si le bouton est sélectionné
+		{
+			buttonText.color = normalColor;
+			isHovered = false;
+		}
+	}
 
-    public void OnSelect(BaseEventData eventData)
-    {
-        buttonText.color = selectedColor;
-        isSelected = true;
-        isHovered = false;
-    }
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		if (!isSelected)
+		{
+			OnSelect(eventData);
+			if (buttonGroup != null)
+			{
+				buttonGroup.OnButtonSelect(this);
+			}
+		}
+	}
 
-    public void OnDeselect(BaseEventData eventData)
-    {
-        buttonText.color = normalColor;
-        isSelected = false;
-        isHovered = false;
+	public void OnSelect(BaseEventData eventData)
+	{
+		buttonText.color = selectedColor;
+		isSelected = true;
+		isHovered = false;
+	}
+
+	public void OnDeselect(BaseEventData eventData)
+	{
+		buttonText.color = normalColor;
+		isSelected = false;
+		isHovered = false;
+	}
+	
+	public void ChangeScene() {
+        SceneManager.LoadScene(sceneName);
     }
 }
